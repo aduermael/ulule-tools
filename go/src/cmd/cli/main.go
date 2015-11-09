2dc695a6f7f18aaba3a0afa2f3d38cba8eb79dc2
Adrien Duermael <adrien@docker.com>
2015-11-08 17:18:26 -0800

    [cmd/cli] get username & apiKey

--- go/src/cmd/cli/main.go
+++ go/src/cmd/cli/main.go
@@ -2,8 +2,62 @@ package main
 
 import (
 	"github.com/GeertJohan/go.linenoise"
+	"github.com/Sirupsen/logrus"
+	"os"
+	"strings"
 )
 
 func main() {
-	linenoise.Line("ulule> ")
+	logrus.Println("---- Ulule CLI ----")
+
+	username := ""
+	apiKey := ""
+
+	// get username from args
+	if len(os.Args) > 1 {
+		username = os.Args[1]
+	}
+
+	// get apiKey from args
+	if len(os.Args) > 2 {
+		apiKey = os.Args[2]
+	}
+
+	var err error
+
+	for username == "" {
+		username, err = linenoise.Line("username> ")
+		if err != nil {
+			username = ""
+		}
+	}
+
+	for apiKey == "" {
+		apiKey, err = linenoise.Line("apiKey> ")
+		if err != nil {
+			apiKey = ""
+		}
+	}
+
+	var completionHandler = func(input string) []string {
+		return []string{"projects"}
+	}
+
+	linenoise.SetCompletionHandler(completionHandler)
+
+	for {
+		cmd, err := linenoise.Line("> ")
+		if err != nil {
+			logrus.Fatal(err)
+		}
+
+		args := strings.Split(cmd, " ")
+
+		if len(args) > 0 {
+			switch args[0] {
+			case "projects":
+
+			}
+		}
+	}
 }
